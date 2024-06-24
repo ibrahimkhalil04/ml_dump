4635b0a6213193ab42a21c16a7871ed23f0b0e01
Ibrahimkhalil04 <ibrahimkhalilt04@gmail.com>
2024-06-24 19:52:02 -0400

    Everything is set! Tests are good compared to the sklearn's model. Will fix the decision boundries plotted by plot_decision_regions() later; there's some issue.

--- DecisionTreeClassifier.py
+++ DecisionTreeClassifier.py
@@ -3,7 +3,9 @@ import numpy as np
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from sklearn.tree import DecisionTreeClassifier as DTClassifier
 
 
 
@@ -20,7 +22,38 @@ X_test_std = sc.transform(X_test)
 
 
 class Node:
-    def __init__(self, features=None, target=None, threshold=None, left=None, right=None, split_index=-1, iG = 0.0): # change the split_inx
+    """
+    A class representing a node in a decision tree.
+
+    Attributes:
+    ----------
+    features : numpy.ndarray
+        The input features for the node.
+    target : numpy.ndarray
+        The target labels for the node.
+    threshold : float
+        The threshold value for splitting the node.
+    left : Node or None
+        The left child node.
+    right : Node or None
+        The right child node.
+    split_index : int
+        The index of the feature used for splitting the node.
+    info_gain : float
+        The information gain achieved by splitting the node.
+
+    Methods:
+    --------
+    split_node():
+        Splits the current node into left and right child nodes based on the threshold.
+    gini_impurity(y: np.array):
+        Computes the Gini impurity for the given target labels.
+    information_gain(criterion):
+        Computes the information gain from splitting the node using the specified criterion.
+    is_pure():
+        Checks if the node is pure (homogeneous).
+    """
+    def __init__(self, features=None, target=None, threshold=0.0, left=None, right=None, split_index=-1, iG = 0.0): # change the split_inx
         self.features = features
         self.target = target
         self.threshold = threshold
@@ -29,51 +62,62 @@ class Node:
         self.split_index = split_index 
         self.info_gain = iG
     
-    
-
     def split_node(self):
-        if self.features is None or self.threshold is None or self.split_index == -1:
+        """
+        Splits the node into left and right child nodes based on the current threshold.
+
+        Returns:
+        --------
+        tuple
+            A tuple containing the left and right child nodes.
+        """
+        if self.features is None or self.threshold == 0.0 or self.split_index == -1:
             return
         lefts = self.features[:, self.split_index] <= self.threshold
         left_features = self.features[lefts]
         right_features = self.features[~lefts]
         left_target = self.target[lefts]
         right_target = self.target[~lefts]
-        return {'features': [left_features, right_features],
-                'targets': [left_target, right_target]}
-
-        #self.left = Node(features=left_features, target=left_target)
-        #self.right = Node(features=right_features, target=right_target)
-        
-        
+        return (Node(features=left_features, target=left_target), Node(features=right_features, target=right_target))
 
     def gini_impurity(self, y: np.array):
-        # Compute the Gini impurity for a list of labels y
+        """
+        Computes the Gini impurity for the given target labels.
+
+        Parameters:
+        -----------
+        y : numpy.ndarray
+            The target labels.
+
+        Returns:
+        --------
+        float
+            The Gini impurity value.
+        """
         uniq, counts = np.unique(y, return_counts=True)
         p = counts / y.size
         gini = 1 - np.sum(p**2)
         return gini
 
-
     def information_gain(self, criterion):
         # Compute the information gain from splitting y into y_left and y_right
-        targets = self.split_node()['targets']
+        left_node, right_node = self.split_node()
         if criterion == 'gini':
             impurity = self.gini_impurity(self.target)
-            impurity_left = self.gini_impurity(targets[0])
-            impurity_right = self.gini_impurity(targets[1])
+            impurity_left = self.gini_impurity(left_node.target) if left_node.target.size > 0 else 0
+            impurity_right = self.gini_impurity(right_node.target) if right_node.target.size > 0 else 0
         elif criterion == 'entropy':
             pass # Will implement later 
         elif criterion == 'error':
             pass # Will implement later 
         else:
             raise ValueError("Invalid criterion. Supported criteria are 'gini', 'entropy', and 'error'.")
-        weight_left = len(targets[0]) / self.target.size
-        weight_right = len(targets[1]) / self.target.size
+        weight_left = left_node.target.size / self.target.size if left_node.target.size > 0 else 0
+        weight_right = right_node.target.size / self.target.size if right_node.target.size > 0 else 0
         return impurity - (weight_left*impurity_left + weight_right*impurity_right)
     
-    def is_pure(self, criterion):
-        if self.info_gain <= 0.0:
+    def is_pure(self):
+        if self.info_gain <= 0.001: # min info gain
             return True
         if np.unique(self.target).size == 1:
             return True
@@ -82,66 +126,99 @@ class Node:
 
 
 
-
 def best_split(node: Node, criterion):
-    # Find the best feature and threshold to split the data
-    rand_split_indx = list(range(node.features[0].size))
-    features = [row for row in node.features]
-    split_indx = -1
-    score = 0.0
-    threshold = None
+    """
+    Finds the best feature index and threshold to split the data based on the specified criterion.
 
-    for i  in rand_split_indx:
-        for row in features:
-            node.split_index = i
-            node.threshold = row[i]
+    Parameters:
+    -----------
+    node : Node
+        The current node in the decision tree where the split is to be evaluated.
+    criterion : str
+        The criterion to use for evaluating the split ('gini', 'entropy', 'error').
+
+    Returns:
+    --------
+    tuple
+        A tuple containing the best feature index, best threshold, and corresponding score (information gain).
+
+    Notes:
+    ------
+    This function iterates over each feature and its unique thresholds to evaluate the information gain
+    achieved by splitting the node. It returns the feature index, threshold, and information gain score
+    that produce the highest gain according to the specified criterion.
+    """
+    best_split_indx = -1
+    best_score = 0.0
+    best_threshold = 0.0
+
+    for idx  in range(node.features.shape[1]):
+        thresholds = np.unique(node.features[:, idx])
+        for threshold in thresholds:
+            node.split_index = idx
+            node.threshold = threshold
             iG = node.information_gain(criterion=criterion)
-            if iG > score:
-                score = iG
-                split_indx = i
-                threshold = row[i]
-    # Out of the loop, split_indx and thereshold correspond to the best feature indx abd threshold to split the node 
-
-    return (split_indx, threshold, score)
-    features = node.split_node()['features']
-    targets = node.split_node()['targets']
-    node.left = Node(features=features[0], target=targets[0], threshold=threshold, split_index=split_indx)
-    node.right = Node(features= features[1], target= targets[1], threshold=threshold, split_index=split_indx)
-
-
+            if iG > best_score:
+                best_score = iG
+                best_split_indx = idx
+                best_threshold = threshold
+    # Out of the loop, best_split_indx, thereshold and score correspond to the best feature index, threshold to split the node 
+    # and the information gain of the node
+    return (best_split_indx, best_threshold, best_score)
 
 
 def build_tree(node: Node, depth=0, max_depth=None, min_samples_split=2, criterion='gini'):
-    # Recursively build the tree
+    """
+    Recursively builds the decision tree by splitting nodes based on the best feature and threshold.
+
+    Parameters:
+    -----------
+    node : Node
+        The current node in the decision tree to be split.
+    depth : int, optional (default=0)
+        The current depth of the node in the tree.
+    max_depth : int or None, optional (default=None)
+        The maximum depth of the tree. If None, grow the tree until all leaves are pure or contain less than min_samples_split samples.
+    min_samples_split : int, optional (default=2)
+        The minimum number of samples required to split an internal node.
+    criterion : str, optional (default='gini')
+        The function to measure the quality of a split ('gini', 'entropy', or 'error').
+
+    Returns:
+    --------
+    Node
+        The root node of the constructed decision tree.
+
+    Notes:
+    ------
+    This function recursively splits nodes by finding the best feature index and threshold to maximize information gain
+    based on the specified criterion. It stops recursion when the stopping criteria (pure node, max depth, or insufficient samples)
+    are met.
+    """
+    if stopping_criteria(node, depth, max_depth, node.target.size, min_samples_split, criterion):
+        return node
     split_indx, threshold, score =  best_split(node, criterion)
-    if split_indx != -1 and threshold is not None:
+    if split_indx != -1 and threshold == 0.0:
         node.split_index = split_indx
         node.threshold = threshold
         node.info_gain = score
-    if stopping_criteria(node, depth, max_depth, node.target.size, min_samples_split, criterion):
-        return node
-    features = node.split_node()['features']
-    targets = node.split_node()['targets']
-    node.left = Node(features=features[0], target=targets[0])
-    node.right = Node(features= features[1], target= targets[1])
-
+    node.left, node.right = node.split_node()
+    # Recursively build the tree for left and right child nodes
     node.left = build_tree(node.left, depth+1, max_depth, min_samples_split, criterion)
     node.right = build_tree(node.right, depth+1, max_depth, min_samples_split, criterion)
     return node
-
-    '''if not stopping_criteria(node.left, depth+1, max_depth, node.left.target.size, min_samples_split, criterion):
-        build_tree(node.left, depth+1, max_depth, min_samples_split, criterion)
-    if not stopping_criteria(node.right, depth+1, max_depth, node.right.target.size, min_samples_split):
-        build_tree(node.right, depth+1, max_depth, min_samples_split, criterion)
-    return'''
 
 
 
 def stopping_criteria(node : Node, depth, max_depth, n_samples, min_samples_split, criterion = 'gini'):
     # Determine if we shuld stop splitting
-    if node.is_pure(criterion) or depth >= max_depth or n_samples <= min_samples_split:
+    if node.is_pure() :
         return True
-    else: return False
+    if depth >= max_depth: 
+        return True
+    if n_samples <= min_samples_split:
+        return True
+    return False
 
 def predict_tree(node: Node, X):
     # Traverse the tree and make predictions for X
@@ -155,6 +232,29 @@ def predict_tree(node: Node, X):
 
 
 class DecisionTreeClassifier:
+    ''' Class representing the decision tree classifier.
+
+    Attributes
+    ----------
+    max_depth : int or None, optional (default=Node)
+        the maximun depth of the tree .
+    min_samples : int, optional (default=2)
+        the minimun number of samples that is required to split a node in the tree.
+    criterion : str, optional (default='gini')
+        The function to measure the quality of a split ('gini', 'entropy', or 'error'). 
+    root : Node
+        The root node of the tree.
+    
+    Methods
+    -------
+    fit(X, y):
+        trains the decision tree classifier on the provided features (X) and  target (y)
+    predict(X):
+        Predicts the target values for the provided features (X) using the trained decision tree.
+    accuracy_score(y_pred, y_true):
+        computes the accuracy score of the model based on predicted (y_pred) and true (y_true) target values.
+    
+    '''
     def __init__(self, max_depth=None, min_samples_split=2, criterion='gini'):
         self.max_depth = max_depth
         self.min_samples_split = min_samples_split
@@ -166,13 +266,13 @@ class DecisionTreeClassifier:
         self.root = build_tree(node, 0, self.max_depth, self.min_samples_split, self.criterion)
 
     def predict(self, X):
-        return [predict_tree(self.root, x) for x in X]
+        return np.array([predict_tree(self.root, x) for x in X])
+    
+    def accuracy_score(self, y_pred: np.array, y_true: np.array):
+        return np.sum(y_pred == y_true) / y_true.size
 
 
-tree_model = DecisionTreeClassifier(max_depth=4)
-tree_model.fit(X_train, y_train)
-
-
+# 
 def print_tree(node: Node, d):
     print('Depth: ', d)
     print('\n')
@@ -183,4 +283,68 @@ def print_tree(node: Node, d):
         print_tree(node.right, d+1)
     return
 
-print_tree(tree_model.root, 0)
+tree_model = DecisionTreeClassifier(max_depth=4)
+tree_model.fit(X_train, y_train)
+predictions =  tree_model.predict(X_test)
+print('Accuracy score : %.3f' % tree_model.accuracy_score(np.array(predictions), y_test))
+
+
+
+
+
+
+
+#*********************************
+def plot_decision_regions(X, y, classifier, resolution= 0.02, test_idx=None):
+    #setup marker generator and color map
+    markers = ['o', 's', '^', 'v', '<']
+    colors = ['red', 'blue', 'lightgreen', 'gray', 'cyan']
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2, = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                            np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.3, cmap= cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+    # plot class examples 
+    for idx, cl, in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0],
+                    y=X[y == cl, 1], 
+                    alpha=0.8, 
+                    c= colors[idx], 
+                    marker= markers[idx], 
+                    label= f'Class {cl}',
+                    edgecolors='black')
+    # Hightlight test examples
+    if test_idx:
+        # plot all examples
+        X_test, y_test = X[test_idx, :], y[test_idx]
+        plt.scatter(X_test[:, 0], X_test[:, 1], c='none', edgecolors='black', 
+                    alpha=1.0, linewidths=1, marker='o', s = 100, label= 'Test set')
+        
+        
+X_combined = np.vstack((X_train_std, X_test_std))
+y_combined = np.hstack((y_train, y_test))
+plot_decision_regions(X=X_combined, y=y_combined, classifier=tree_model, test_idx=range(105, 150))
+plt.xlabel("Petal length [cm]")
+plt.ylabel("petal width [cm]")
+plt.title('Trained with my own implementation of a decision tree model')
+plt.legend(loc='best')
+plt.tight_layout()
+plt.show()
+
+sk_tree_model = DTClassifier(criterion='gini', max_depth=4, random_state=1)
+sk_tree_model.fit(X_train, y_train)
+X_combined = np.vstack((X_train_std, X_test_std))
+y_combined = np.hstack((y_train, y_test))
+plot_decision_regions(X=X_combined, y=y_combined, classifier=sk_tree_model, test_idx=range(105, 150))
+plt.xlabel("Petal length [cm]")
+plt.ylabel("petal width [cm]")
+plt.title('Trained with sklearn\'s decision tree model')
+plt.legend(loc='best')
+plt.tight_layout()
+plt.show()
