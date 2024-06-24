54aaa2ca97e3d6355bc7558ae73afa875502b284
Ibrahimkhalil04 <ibrahimkhalilt04@gmail.com>
2024-06-24 12:23:42 -0400

    Nearly everything is set. Just have to do debug a little bit and fix a problem

--- DecisionTreeClassifier.py
+++ DecisionTreeClassifier.py
@@ -1,5 +1,22 @@
 
 import numpy as np 
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+
+
+
+# Prep the data
+iris = datasets.load_iris()
+X = iris.data[:, [2, 3]]
+y = iris.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=1, stratify=y)
+sc = StandardScaler()
+sc.fit(X_train)
+X_train_std = sc.transform(X_train)
+X_test_std = sc.transform(X_test)
+
 
 
 class Node:
@@ -14,15 +31,19 @@ class Node:
     
 
     def split_node(self):
-        if self.features == None or self.threshold == None or self.split_index == max:
+        if self.features is None or self.threshold is None or self.split_index == -1:
             return
         lefts = self.features[:, self.split_index] <= self.threshold
         left_features = self.features[lefts]
         right_features = self.features[~lefts]
         left_target = self.target[lefts]
         right_target = self.target[~lefts]
-        self.left = Node(features=left_features, target=left_target)
-        self.right = Node(features=right_features, target=right_target)
+        return {'features': [left_features, right_features],
+                'targets': [left_target, right_target]}
+
+        #self.left = Node(features=left_features, target=left_target)
+        #self.right = Node(features=right_features, target=right_target)
+        
         
 
     def gini_impurity(self, y: np.array):
@@ -35,19 +56,19 @@ class Node:
 
     def information_gain(self, criterion):
         # Compute the information gain from splitting y into y_left and y_right
-        self.split_node()
+        targets = self.split_node()['targets']
         if criterion == 'gini':
             impurity = self.gini_impurity(self.target)
-            impurity_left = self.gini_impurity(self.left.target)
-            impurity_right = self.gini_impurity(self.right.target)
+            impurity_left = self.gini_impurity(targets[0])
+            impurity_right = self.gini_impurity(targets[1])
         elif criterion == 'entropy':
             pass # Will implement later 
         elif criterion == 'error':
             pass # Will implement later 
         else:
             raise ValueError("Invalid criterion. Supported criteria are 'gini', 'entropy', and 'error'.")
-        weight_left = self.left.target.size / self.target.size
-        weight_right = self.right.target.size / self.target.size
+        weight_left = len(targets[0]) / self.target.size
+        weight_right = len(targets[1]) / self.target.size
         return impurity - (weight_left*impurity_left + weight_right*impurity_right)
     
     def is_pure(self, criterion):
@@ -61,21 +82,61 @@ class Node:
 
 
 
-def best_split(X, y, criterion):
+def best_split(node: Node, criterion):
     # Find the best feature and threshold to split the data
-    pass
+    rand_split_indx = list(range(node.features[0].size))
+    features = [row for row in node.features]
+    split_indx = -1
+    score = 0.000001
+    threshold = None
 
-def build_tree(X, y, depth=0, max_depth=None, min_samples_split=2, criterion='gini'):
+    for i  in rand_split_indx:
+        for row in features:
+            node.split_index = i
+            node.threshold = row[i]
+            iG = node.information_gain(criterion=criterion)
+            if iG > score:
+                score = iG
+                split_indx = i
+                threshold = row[i]
+    # Out of the loop, split_indx and thereshold correspond to the best feature indx abd threshold to split the node 
+    node.split_index = split_indx
+    node.threshold = threshold
+    features = node.split_node()['features']
+    targets = node.split_node()['targets']
+    node.left = Node(features=features[0], target=targets[0], threshold=threshold, split_index=split_indx)
+    node.right = Node(features= features[1], target= targets[1], threshold=threshold, split_index=split_indx)
+
+
+
+
+def build_tree(node: Node, depth=0, max_depth=None, min_samples_split=2, criterion='gini'):
     # Recursively build the tree
-    pass
+    best_split(node, criterion)
+    if not stopping_criteria(node.left, depth+1, max_depth, node.left.target.size, min_samples_split, criterion):
+        build_tree(node.left, depth+1, max_depth, min_samples_split, criterion)
+    if not stopping_criteria(node.right, depth+1, max_depth, node.right.target.size, min_samples_split):
+        build_tree(node.right, depth+1, max_depth, min_samples_split, criterion)
+    return
 
-def stopping_criteria(depth, max_depth, n_samples, min_samples_split):
+
+
+def stopping_criteria(node : Node, depth, max_depth, n_samples, min_samples_split, criterion = 'gini'):
     # Determine if we should stop splitting
-    pass
+    if node.is_pure(criterion) or depth >= max_depth or n_samples <= min_samples_split:
+        return True
+    else: return False
 
-def predict_tree(node, X):
+def predict_tree(node: Node, X):
     # Traverse the tree and make predictions for X
-    pass
+    if node.is_pure():
+        uniq, counts = np.unique(node.target, return_counts=True)
+        return uniq[counts.argmax()]
+    if X[node.split_index] <= node.threshold:
+        return predict_tree(node.left, X)
+    else:
+        return predict_tree(node.right, X)
+
 
 class DecisionTreeClassifier:
     def __init__(self, max_depth=None, min_samples_split=2, criterion='gini'):
@@ -85,7 +146,12 @@ class DecisionTreeClassifier:
         self.root = None
 
     def fit(self, X, y):
-        self.root = build_tree(X, y, 0, self.max_depth, self.min_samples_split, self.criterion)
+        node = Node(X, y)
+        self.root = build_tree(node, 0, self.max_depth, self.min_samples_split, self.criterion)
 
     def predict(self, X):
         return [predict_tree(self.root, x) for x in X]
+
+
+tree_model = DecisionTreeClassifier(max_depth=4)
+tree_model.fit(X_train, y_train)
